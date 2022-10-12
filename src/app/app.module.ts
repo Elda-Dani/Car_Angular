4f213a9570ef3aab2eb9d04381be67ad33238c4f
Elda-Dani <eldadani2001@gmail.com>
2022-10-12 22:29:48 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ListsComponent } from './lists/lists.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes :Routes=[
+  {
+    path:"",component:ListsComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
